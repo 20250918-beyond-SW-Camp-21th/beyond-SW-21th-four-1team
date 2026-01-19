3bd104695e2072f3d9c884b2842a1bfb60c60086
rlatjddms <kseo_o@naver.com>
2026-01-19 15:46:57 +0900

    [chore] jenkins 테스트를 위한 누락된 설정 추가

--- backend/src/test/java/com/spicy/backend/settlement/api/SettlementControllerIntegrationTests.java
+++ backend/src/test/java/com/spicy/backend/settlement/api/SettlementControllerIntegrationTests.java
@@ -1,6 +1,5 @@
 package com.spicy.backend.settlement.api;
 
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @Transactional
 class SettlementControllerIntegrationTests {
 
