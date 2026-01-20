b2418320c3a93091695aa794cdc67b1b5394032a
rlatjddms <kseo_o@naver.com>
2026-01-20 18:00:45 +0900

    [refactor] SecurityConfig 수정

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance
 const api = axios.create({
-    baseURL: '/api/v1',
+    baseURL: 'http://localhost:8080/api/v1',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
